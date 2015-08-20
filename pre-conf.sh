ee045bdbb14efce992dae04b101d523638b6dce0
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-20 18:20:27 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,6 +10,10 @@
 
  echo "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
 
+ rm -R /var/www/html
+ #to fix error relate to ip address of container apache2
+ echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
+ ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
 
 killall mysqld
 sleep 10s
