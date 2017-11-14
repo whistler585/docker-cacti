cd1acc9705817567b880cf3dfca0dcc728c9d55c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-14 09:15:29 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 /usr/bin/mysqld_safe &
- sleep 10s
+ 
+mkdir /opt/logs
+touch /opt/logs/cacti.log
+touch /opt/logs/httpd_access.log
+touch /opt/logs/httpd_error.log
+chown -R www-data /opt/logs/*
 
  mysqladmin -u root password mysqlpsswd
  mysqladmin -u root -pmysqlpsswd reload
  mysqladmin -u root -pmysqlpsswd create cacti
- mysql -u root -pmysqlpsswd cacti < /usr/share/cacti/conf_templates/cacti.sql
+ mysql -u root -pmysqlpsswd cacti < /opt/cacti/cacti.sql
+ 
+ mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -pmysqlpsswd mysql
 
  echo "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
+ echo "GRANT SELECT ON mysql.time_zone_name TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
 
  rm -R /var/www/html
  
