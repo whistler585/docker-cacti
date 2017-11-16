2590b97a09dbceb7c881deaaba7d72fd7f064ae3
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-15 21:42:28 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,6 @@ else
       #code that need to run only one time ....
       
         #to fix problem with data.timezone that appear at 1.28.108 for some reason
-        echo $TZ > /etc/timezone
         sed  -i "s|\;date.timezone =|date.timezone = \"${TZ:-America/New_York}\"|" /etc/php/7.0/apache2/php.ini
       
         #needed for fix problem with ubuntu and cron
