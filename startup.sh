e86ad724385cc63e5820bb85d7b264f8b22920cf
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-16 01:03:15 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ else
       
         #to fix problem with data.timezone that appear at 1.28.108 for some reason
         sed  -i "s|\;date.timezone =|date.timezone = \"${TZ:-America/New_York}\"|" /etc/php/7.0/apache2/php.ini
-      
+        echo 'default-time-zone = '$TZ >> /etc/mysql/my.cnf
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
