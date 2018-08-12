e287bfa4d267cd36a147fda2789e05d3239b175f
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-08-12 01:15:30 -0400

    Update snmpd.sh

--- snmpd.sh
+++ snmpd.sh
@@ -3,4 +3,4 @@
 # `chpst -u snmp` runs the given command as the user `xxxxx`.
 # If you omit that part, the command will be run as root.
 
-exec chpst -u root /usr/sbin/snmpd -f 2>&1
+exec chpst -u Debian-snmp /usr/sbin/snmpd -f 2>&1
